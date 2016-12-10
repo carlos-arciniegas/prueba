1c63ed68c83761277779854caa421cd6bdd8870b
carlos-arciniegas <carlosarc92@gmail.com>
2016-12-09 22:29:33 -0400

    primero

---
+++ Reserva-Develop/src/BOReserva/Scripts/cargadorMenu.js
@@ -0,0 +1,363 @@
+﻿jQuery(document).ready(function () {
+
+    $("#gestionAviones").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_aviones/M02_GestionAviones';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty()
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#m02_agregaravion").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_aviones/M02_AgregarAvion';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+    
+    $("#m08AgregarAutomovil").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_automoviles/M08_AgregarAutomovil';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+
+    });
+    $("#m08VisualizarAutomoviles").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_automoviles/M08_VisualizarAutomoviles';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+
+    });
+
+    //INICIO M06_Gestion_Comida
+    $("#gestionComida").click(function (e) {
+        //M06_AgregarComida
+        e.preventDefault();
+        var url = '/gestion_comida_vuelo/M06_AgregarComida';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+    $("#editarComida").click(function (e) {
+        //M06_EditarComida
+        e.preventDefault();
+        var url = '/gestion_comida_vuelo/M06_EditarComida';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+    $("#gestionComidaVuelo").click(function (e) {
+        //M06_AgregarPorVuelo
+        e.preventDefault();
+        var url = '/gestion_comida_vuelo/M06_AgregarPorVuelo';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                }
+            });
+    });
+    //FIN M06_Gestion_Comida
+
+
+
+    $("#crearhotel").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_hoteles/M09_GestionHoteles_Crear';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+    $("#modificarhotel").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_hoteles/M09_GestionHoteles_ModificarHotel';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });   
+
+
+    $("#m05_crearboleto").click(function (e) {
+        e.preventDefault();
+        var url = '/gestion_boletos/M05_CrearBoleto';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    //INICIO M11_Gestion_Ofertas_Paquetes
+    $("#agregarPaquete").click(function (e) {
+        //M11_AgregarPaquete
+        e.preventDefault();
+        var url = '/gestion_Ofertas/M11_AgregarPaquete';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#agregarOferta").click(function (e) {
+        //M11_AgregarOferta
+        e.preventDefault();
+        var url = '/gestion_Ofertas/M11_AgregarOferta';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#consultarOferta").click(function (e) {
+        //M11_AgregarOferta
+        e.preventDefault();
+        var url = '/gestion_Ofertas/M11_ConsultarOferta';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#consultarPaquete").click(function (e) {
+        //M11_AgregarOferta
+        e.preventDefault();
+        var url = '/gestion_Ofertas/M11_ConsultarPaquete';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#modificarPaquete").click(function (e) {
+        //M11_AgregarOferta
+        e.preventDefault();
+        var url = '/gestion_Ofertas/M11_ModificarPaquete';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+
+    $("#modificarOferta").click(function (e) {
+        //M11_AgregarOferta
+        e.preventDefault();
+        var url = '/gestion_ofertas/M11_ModificarOferta';
+        var method = 'GET';
+        var data = '';
+
+        $.ajax(
+            {
+                url: url,
+                type: method,
+                data: data,
+                success: function (data, textStatus, jqXHR) {
+
+                    $("#contenido").empty();
+                    $("#contenido").append(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    alert(errorThrown);
+                }
+            });
+    });
+});
